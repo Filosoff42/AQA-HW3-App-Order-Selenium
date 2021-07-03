43141b43ab71eaad2e9501d125bf6388a9278129
Grigoriy Kharitonskiy <argh94@list.ru>
2021-07-03 12:56:26 +0300

    Initial commit

---
+++ src/test/java/AppOrderTest.java
@@ -0,0 +1,41 @@
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class AppOrderTest {
+    private WebDriver driver;
+
+    @BeforeAll
+    static void setUpAll() {
+        System.setProperty("webdriver.gecko.driver", "./driver/win/geckodriver.exe");
+    }
+
+    @BeforeEach
+    void setUp() {
+        FirefoxOptions options = new FirefoxOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new FirefoxDriver(options);
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    void shouldTestSomething() throws InterruptedException {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Григорий Харитонский");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+70000000000");
+        driver.findElement(By.cssSelector("[data-test-id=agreement] span")).click();
+        driver.findElement(By.cssSelector("button")).click();
+        String text = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
+    }
+}
